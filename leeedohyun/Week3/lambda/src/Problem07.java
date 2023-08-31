389422bb10c1905ebecacca2a3de976b12ad7af8
leeedohyun <ldh19738@gmail.com>
2023-08-31 15:55:55 +0900

    docs: 하나의 리스트로 처리하도록 수정

--- leeedohyun/Week3/lambda/src/Problem07.java
+++ leeedohyun/Week3/lambda/src/Problem07.java
@@ -4,12 +4,8 @@ import java.util.stream.Collectors;
 
 public class Problem07 {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 2, 2, 5, 9, 10);
-
-        List<Integer> collect = numbers.stream()
+        Arrays.stream(new int[] {1, 2, 3, 4, 5, 2, 2, 5, 9, 10})
                 .distinct()
-                .collect(Collectors.toList());
-
-        System.out.println(collect);
+                .forEach(System.out::println);
     }
 }
